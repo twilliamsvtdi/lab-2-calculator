e06a68fdb671dca070e341bc1ea341d3909b6421
Trevoir Williams <trevoir.williams@gmail.com>
2019-02-01 23:13:18 -0500

    initial commit. includes the functional buttons and code for a calculator.

--- lab_1_calculator/Form1.Designer.cs
+++ lab_1_calculator/Form1.Designer.cs
@@ -28,13 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbNum1 = new System.Windows.Forms.TextBox();
+            this.tbNum2 = new System.Windows.Forms.TextBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnPlus);
+            this.flowLayoutPanel1.Controls.Add(this.btnMinus);
+            this.flowLayoutPanel1.Controls.Add(this.btnMultiply);
+            this.flowLayoutPanel1.Controls.Add(this.btnDivide);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(201, 212);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(396, 100);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlus.Location = new System.Drawing.Point(3, 3);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(92, 82);
+            this.btnPlus.TabIndex = 2;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinus.Location = new System.Drawing.Point(101, 3);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(97, 82);
+            this.btnMinus.TabIndex = 2;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(204, 3);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(97, 82);
+            this.btnMultiply.TabIndex = 3;
+            this.btnMultiply.Text = "X";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivide.Location = new System.Drawing.Point(307, 3);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(86, 82);
+            this.btnDivide.TabIndex = 4;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(145, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "First Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(145, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Second Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(234, 353);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Result";
+            // 
+            // tbNum1
+            // 
+            this.tbNum1.Location = new System.Drawing.Point(377, 61);
+            this.tbNum1.Name = "tbNum1";
+            this.tbNum1.Size = new System.Drawing.Size(204, 22);
+            this.tbNum1.TabIndex = 5;
+            // 
+            // tbNum2
+            // 
+            this.tbNum2.Location = new System.Drawing.Point(377, 110);
+            this.tbNum2.Name = "tbNum2";
+            this.tbNum2.Size = new System.Drawing.Size(204, 22);
+            this.tbNum2.TabIndex = 6;
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(380, 361);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(204, 22);
+            this.tbResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbNum2);
+            this.Controls.Add(this.tbNum1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Trev\'s Calculator";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbNum1;
+        private System.Windows.Forms.TextBox tbNum2;
+        private System.Windows.Forms.TextBox tbResult;
     }
 }
 
--- lab_1_calculator/Form1.cs
+++ lab_1_calculator/Form1.cs
@@ -16,5 +16,72 @@ namespace lab_1_calculator
         {
             InitializeComponent();
         }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            if (!isTextboxEmpty())
+            {
+                var num1 = int.Parse(tbNum1.Text);
+                var num2 = int.Parse(tbNum2.Text);
+                tbResult.Text = (num1 + num2).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please that all values are valid!");
+            }
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            if (!isTextboxEmpty())
+            {
+                var num1 = int.Parse(tbNum1.Text);
+                var num2 = int.Parse(tbNum2.Text);
+                tbResult.Text = (num1 - num2).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please that all values are valid!");
+            }
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            if (!isTextboxEmpty())
+            {
+                var num1 = int.Parse(tbNum1.Text);
+                var num2 = int.Parse(tbNum2.Text);
+                tbResult.Text = (num1 * num2).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please that all values are valid!");
+            }
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            if (!isTextboxEmpty() && !isDivisorZero())
+            {
+                var num1 = int.Parse(tbNum1.Text);
+                var num2 = int.Parse(tbNum2.Text);
+                tbResult.Text = (num1 + num2).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please that all values are valid!");
+            }
+        }
+
+        bool isTextboxEmpty()
+        {
+            return String.IsNullOrEmpty(tbNum1.Text) || 
+                String.IsNullOrEmpty(tbNum1.Text);
+        }
+
+        bool isDivisorZero()
+        {
+            return int.Parse(tbNum2.Text) == 0;
+        }
     }
 }
